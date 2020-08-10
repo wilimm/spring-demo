c365fe202e0787b8f7792828e465ba534ac2f4ae
meikai <meikai@wumii.com>
2020-08-10 18:21:25 +0800

    add ch08

---
+++ src/main/java/com/wilimm/ch08/Demo.java
@@ -0,0 +1,10 @@
+package com.wilimm.ch08;
+
+import lombok.Data;
+
+public class Demo {
+
+    public static void main(String[] args) throws Exception {
+		System.out.println("test demo");
+    }
+}
